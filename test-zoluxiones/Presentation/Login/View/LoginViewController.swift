85dad7e25595a3e4cfd460868ca14f481046f56c
kiwik <jusepe600@msn.com>
2022-05-09 15:06:35 -0500

    Create Login view

---
+++ test-zoluxiones/Presentation/Login/View/LoginViewController.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewController.swift
+//  test-zoluxiones
+//
+//  Created by Julio Perez on 9/05/22.
+//
+
+import Foundation
---
+++ test-zoluxiones/Presentation/Login/ViewModel/LoginViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewModel.swift
+//  test-zoluxiones
+//
+//  Created by Julio Perez on 9/05/22.
+//
+
+import Foundation
